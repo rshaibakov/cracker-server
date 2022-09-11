6ad6f0f2391d21799fea37f82e9c03b32ea5a781
Ramil Shaybakov <rshaibakov@gmail.com>
2022-09-11 21:53:14 +0500

    chore(mongo): connect to db

--- src/index.ts
+++ src/index.ts
@@ -1,20 +1,36 @@
 import dotenv from 'dotenv'
 import fastify from 'fastify'
+import mongoose from 'mongoose'
 
 dotenv.config()
-console.log(process.env)
 
-const server = fastify()
+const {
+  DB_PROTOCOL = 'mongodb',
+  DB_HOST = 'localhost',
+  DB_PORT = '27017',
+  DB_NAME = 'test',
+  DB_USERNAME = 'test',
+  DB_PASSWORD = ''
+} = process.env
+const DB_URI = `${DB_PROTOCOL}://${DB_USERNAME}:${DB_PASSWORD}@${DB_HOST}:${DB_PORT}/${DB_NAME}`
 
-server.get('/ping', async (request, reply) => {
-  return 'pong\n'
-})
+init().catch(err => console.log(err))
 
-server.listen({ port: 8080 }, (err, address) => {
-  if (err != null) {
-    console.error(err)
-    process.exit(1)
-  }
+async function init (): Promise<void> {
+  await mongoose.connect(DB_URI)
 
-  console.log(`Server listening at ${address}`)
-})
+  const server = fastify()
+
+  server.get('/ping', async (request, reply) => {
+    return 'pong\n'
+  })
+
+  server.listen({ port: 8080 }, (err, address) => {
+    if (err != null) {
+      console.error(err)
+      process.exit(1)
+    }
+
+    console.log(`Server listening at ${address}`)
+  })
+}
